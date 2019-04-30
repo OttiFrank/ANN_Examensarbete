7d0d03efa2200888fdd6f6a50fe7f4c264f1c76c
Martin Leijon <martinleeijon@gmail.com>
2019-04-30 12:48:37 +0200

    Update ANN_Examensarbete.py

--- ANN_Examensarbete/ANN_Examensarbete.py
+++ ANN_Examensarbete/ANN_Examensarbete.py
@@ -72,17 +72,13 @@ print(train_X[0].shape)
 
 # design network
 model = Sequential()
-model.add(LSTM(8, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
+model.add(LSTM(64, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
 
-model.add(LSTM(16, return_sequences=True, activation='relu'))
-#model.add(Dropout(0.2))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
 
-model.add(LSTM(32, return_sequences=True, activation='relu'))
-#model.add(Dropout(0.2))
+model.add(LSTM(64, activation='relu'))
 
-model.add(LSTM(128))
-
-model.add(Dense(1, activation='linear'))
+model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 print(train_X.shape)
